22d40573a88dad1605588c0a6338700735262ea0
MisterSeph <josephleakey@gmail.com>
2017-02-01 10:48:47 +0000

    Extended PlotEffect class and created PlayerEffect class

--- core/src/io/github/teamfractal/entity/PlotEffect.java
+++ core/src/io/github/teamfractal/entity/PlotEffect.java
@@ -1,14 +1,11 @@
 package io.github.teamfractal.entity;
 
+import com.badlogic.gdx.utils.Array;
+
 /**
  * Created by Joseph on 31/01/2017.
  */
-public class PlotEffect {
-
-    /**
-     * The plot that the object affects
-     */
-    private LandPlot plot;
+public class PlotEffect extends Array<Integer[]> {
 
     /**
      * The name of the effect
@@ -21,61 +18,71 @@ public class PlotEffect {
     private String description;
 
     /**
-     * Array holding the production modifiers that the effect imposes
+     * Constructor that assigns a name and a description to the effect
+     *
+     * @param name The name of the effect
+     * @param description A description of the effect
      */
-    private int[] newModifiers;
-
-    /**
-     * Used as temporary storage for plot-modifiers that this class replaces
-     */
-    private int[] originalModifiers;
-
-    /**
-     * Determines whether the modifiers stored in this class are to be applied to those already affecting the bound
-     * plot, or if they're meant to replace said plot's original modifiers entirely
-     */
-    private boolean overwrite;
-
-    /**
-     * Determines whether or not the effect is active
-     */
-    private boolean active;
-
-    public PlotEffect(LandPlot plot, String name, String description, int[] productionModifiers, boolean overwrite) {
-        this.plot = plot;
-        //Assigns the effect to a plot
-
+    public PlotEffect(String name, String description) {
         this.name = name;
         this.description = description;
         //Stores the effect's name and description for future reference
-
-        this.newModifiers = productionModifiers;
-
-        this.overwrite = overwrite;
-
-        active = false;
     }
 
-    public void switchActive() {
-        if (active == false) {
-            originalModifiers = plot.productionModifiers;
+    /**
+     * Imposes the effect's modifiers on the provided plot
+     * If you ever want to restore a tile's original modifiers after using this method to change them, call this
+     * again on the same tile and specify mode 2
+     *
+     * @param plot The plot to be affected
+     * @param mode The mode of effect [0: ADD | 1: MULTIPLY | 2: OVERWRITE]
+     * @param save Keeps the current modifiers at the top of the stack if true
+     */
+    public void impose(LandPlot plot, int mode, boolean save) {
+        Integer[] originalModifiers = new Integer[3];
+        Integer[] newModifiers;
+        //Declare temporary arrays to handle modifier modifications
 
-            if (overwrite == true) {
-                plot.productionModifiers = newModifiers;
-            } else {
-                for (int i = 0; i < plot.productionModifiers.length; i++) {
+        newModifiers = super.pop();
+        //Assume that the modifiers on the top of the stack are the modifiers to be imposed
+
+        for (int i = 0; i < 3; i++) {
+            originalModifiers[i] = plot.productionModifiers[i];
+            //Save each of the specified tile's original modifiers
+
+            switch (mode) {
+                case (0):
+                    plot.productionModifiers[i] += newModifiers[i];
+                    //MODE 0: Add/subtract to/from the original modifiers
+                    break;
+                case (1):
                     plot.productionModifiers[i] *= newModifiers[i];
-                }
+                    //MODE 1: Multiply the original modifiers
+                    break;
+                case (2):
+                    plot.productionModifiers[i] = newModifiers[i];
+                    //MODE 2: Replace the original modifiers
+                    break;
             }
-        } else {
-            plot.productionModifiers = originalModifiers;
         }
 
-        active = !active;
+        super.add(originalModifiers);
+        //Add the tile's original modifiers to the stack for later access...
+
+        if (save == true) {
+            super.add(newModifiers);
+        }
+        //...and return the imposed modifiers to the top of the stack if needs be
     }
 
-    public Boolean active() {
-        return active;
+    public void swapTop() {
+        if (super.size > 1) {
+            Integer[] i = super.pop();
+            Integer[] j = super.pop();
+
+            super.add(i);
+            super.add(j);
+        }
     }
 
     public String name() {
