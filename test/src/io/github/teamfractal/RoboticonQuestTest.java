8a5a7b33d2d2ab4eea869444ea61ecdb30f43e9c
NotKieran <kjh532@york.ac.uk>
2017-02-17 13:52:03 +0000

    Solved some TODOs

--- test/src/io/github/teamfractal/RoboticonQuestTest.java
+++ test/src/io/github/teamfractal/RoboticonQuestTest.java
@@ -1,18 +1,20 @@
 package io.github.teamfractal;
-import org.junit.Before;
+
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
-
+//TODO: test these
 public class RoboticonQuestTest extends TesterFile {
-	private RoboticonQuest game;
-	//TODO: Find a different way to test
-	@Before
-	public void setUp(){
-		game = new RoboticonQuest();
-		game.reset(true);
-		
-	}
+    private RoboticonQuest game = new RoboticonQuest();
+
+    public RoboticonQuestTest() {
+        game.create();
+        game.setScreen(game.gameScreen);
+        game.gameScreen.newGame(false);
+    }
+
+
 	@Test
 	public void phaseTest(){
 		assertEquals(0, game.getPhase());  //Test game starts in correct phase
@@ -20,11 +22,11 @@ public class RoboticonQuestTest extends TesterFile {
 			game.nextPhase();
 			assertEquals(i, game.getPhase());  //Test game will move from stages 1 to 5 without problem
 		}
-		game.nextPhase(); 
-		assertEquals(1, game.getPhase()); //Test phase resets
-	}
-	
-	@Test
+        game.nextPhase();
+        assertEquals(1, game.getPhase()); //Test phase resets
+    }
+
+    @Test
 	public void playerTest(){
 		assert(game.getPlayerInt() == 0);  //Test game starts in correct player
 		game.nextPlayer();
