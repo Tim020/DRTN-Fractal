fe8d52ffca06c09d2f53ffc4cfef64be7b190fb6
MisterSeph <josephleakey@gmail.com>
2017-02-17 00:22:15 +0000

    Added window icon and title

--- desktop/src/io/github/teamfractal/desktop/DesktopLauncher.java
+++ desktop/src/io/github/teamfractal/desktop/DesktopLauncher.java
@@ -3,6 +3,8 @@
 
 package io.github.teamfractal.desktop;
 
+import com.badlogic.gdx.Files;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import io.github.teamfractal.RoboticonQuest;
@@ -11,6 +13,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
+		config.title = "Duck-Related Roboticon Quest";
+		config.addIcon("icon.png", Files.FileType.Internal);
 		config.backgroundFPS = 1;
 		config.vSyncEnabled = true;
 		config.width = 1024;
