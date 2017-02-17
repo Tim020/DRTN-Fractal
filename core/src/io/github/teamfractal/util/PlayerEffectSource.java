928a72243a1c43102006860b979b27f5d0bfe93d
Martynas <mm1544@york.ac.uk>
2017-02-17 14:05:42 +0000

    player effect

--- core/src/io/github/teamfractal/util/PlayerEffectSource.java
+++ core/src/io/github/teamfractal/util/PlayerEffectSource.java
@@ -12,6 +12,7 @@ public class PlayerEffectSource extends Array<PlayerEffect> {
     private RoboticonQuest engine;
 
     private PlayerEffect partyHard;
+    private PlayerEffect vikingRaid;
 
     public PlayerEffectSource(final RoboticonQuest engine) {
         this.engine = engine;
@@ -25,9 +26,13 @@ public class PlayerEffectSource extends Array<PlayerEffect> {
                 "you're a capitalist and your money\nis worthless to you. Unfortunately, you got too drunk and " +
                 "attempted to use some your fat stacks as Cards\nAgainst Humanity by scrawling immature statements " +
                 "all over them with a permanent marker, thereby\nrendering them worthess.\n\n-30 Money", 0, 0, 0, -30, false);
+
+        vikingRaid = new PlayerEffect("Viking Raid", "You have been raided by a group of intergalactical Vikings." +
+                "They took:\n\n-10 Ore -10 Energy -10 Food and -10 Money", 0, 0, 0, -10, false);
     }
 
     public void implementEffects() {
         add(partyHard);
+        add(vikingRaid);
     }
 }
