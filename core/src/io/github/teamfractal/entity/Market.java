294484cb0d5507f23111fa5516391d574fc02768
Martynas <mm1544@york.ac.uk>
2017-02-20 13:01:57 +0000

    header comment for changed classes added

--- core/src/io/github/teamfractal/entity/Market.java
+++ core/src/io/github/teamfractal/entity/Market.java
@@ -1,3 +1,16 @@
+/**
+ * @author DRTN
+ * Team Website with download:
+ * https://misterseph.github.io/DuckRelatedFractalProject/
+ *
+ * This Class contains either modifications or is entirely new in Assessment 3
+ *
+ * If you are in any doubt a complete changelog can be found here:
+ * https://github.com/NotKieran/DRTN-Fractal/compare/Fractal_Initial...development
+ *
+ * And a more concise report can be found in our Change3 document.
+ **/
+
 package io.github.teamfractal.entity;
 
 import io.github.teamfractal.entity.enums.ResourceType;
